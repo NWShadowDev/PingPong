a8438f21b5039dbda58b0c2066024d57465f2b21
Amber Farrington and Karen Freeman-Smith <amber@>
2016-10-10 08:43:44 -0700

    add new JS files, separate front and backend into different files, add json package

---
+++ js/pingpong.js
@@ -0,0 +1,22 @@
+function Calculator (skinName) {
+  this.skin = skinName;
+}
+
+
+Calculator.prototype.pingPong = function(number){
+    message = [];
+  for (var i = 1; i <= number; i ++) {
+    if ((i % 5 === 0)&&(i % 3 === 0)){
+      message.push("ping pong");
+    } else if (i % 3 === 0){
+      message.push("ping");
+    } else if (i % 5 === 0){
+      message.push("pong");
+    }else {
+      message.push(i);
+    }
+  };
+  return message;
+};
+
+exports.calculatorModule = Calculator;
